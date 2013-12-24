63e617699223e4011412ad3e5f9ce0b082d9e5e4
kevin Wurster <wursterk@gmail.com>
2013-12-23 23:22:48 -0500

    Initial development

---
+++ bin/desktop.py
@@ -0,0 +1,24 @@
+import sys
+import fsnav
+
+
+# Build information
+__author__ = 'Kevin Wurster'
+__version__ = '0.1'
+__email__ = 'wursterk@gmail.com'
+__license__ = 'See LICENSE.txt'
+
+
+# Global variables
+_ScriptName = sys.argv[0]
+
+
+def main():
+
+    if fsnav.desktop(mode='cd') is not True:
+        print("%s ERROR: Could not cd to: %s" % (_ScriptName, str(fsnav.desktop(mode='return'))))
+
+
+if __name__ == '__main__':
+    main()
+    exit()
