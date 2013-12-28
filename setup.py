c6ac384a3f77bec60ff3c51c9a9b75bd2168c49b
Kevin Wurster <wursterk@gmail.com>
2013-12-27 21:45:00 -0500

    Initial development

--- setup.py
+++ setup.py
@@ -1,41 +1,23 @@
 #!/usr/bin/env python
 
 
-import os
-import sys
-import site
-from sys import exit
 from glob import glob
-from os.path import sep
+from distutils.core import setup
 
 
 # Build information
 __author__ = 'Kevin Wurster'
 __version__ = '0.1'
 __email__ = 'wursterk@gmail.com'
-__license__ = 'See LICENSE.txt'
+__license__ = 'See LICENSE.txt from original distribution'
 
 
-def print_help():
-    pass
-
-
-def print_usage():
-    pass
-
-
-def print_version():
-    pass
-
-
-def main(args):
-
-    # Define defaults
-    src_code = 'src' + sep + 'fsnav.py'
-    utilities = [i for i in glob('src' + sep + '*') if '__init__' not in i]  # init file is for testing only
-    site_packages = site.getsitepackages()[0]
-
-
-
-if __name__ == '__main__':
-    main(sys.argv)
+setup(name='FS_Nav',
+      version='0.1',
+      description='Simple framework and tools for navigating the command line',
+      author=__author__,
+      author_email=__email__,
+      url='https://github.com/geowurster/FS_Nav',
+      packages='fsnav.py',
+      scripts=glob('scripts' + '*')
+      )
