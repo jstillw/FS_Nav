d40fa4f6a93ee447d6432cc295b998fc55fe119c
kevin Wurster <wursterk@gmail.com>
2014-01-30 22:52:35 -0500

    Initial development

--- bin/fsnav_linker.sh
+++ bin/fsnav_linker.sh
@@ -1,33 +1,75 @@
 #!/bin/bash
 
+
 # Build information
 __AUTHOR__='Kevin Wurster'
 __VERSION__='0.1'
 __EMAIL__='wursterk@gmail.com'
+__LICENSE__='See LICENSE.txt from original package'
 __SOURCE__='https://github.com/geowurster/FS_Nav'
 
 
 # Functions to print out helpful information
 PRINT_HELP_INFO() {
-    echo "Help info"
-    exit
+    echo ""
+    echo "The following flags contain help information:"
+    echo "  --help-info"
+    echo "  --version"
+    echo "  --license"
+    echo "  --help"
+    echo ""
+    exit 1
 }
 
 PRINT_HELP() {
+    echo ""
     echo "Help"
-    exit
+    echo ""
+    exit 1
 }
 
 PRINT_VERSION() {
+    echo ""
     echo "Version"
-    exit
+    echo ""
+    exit 1
 }
 
 PRINT_LICENSE() {
     echo ""
-    echo ${__SOURCE__}
+    echo ${__LICENSE__}
     echo ""
-    exit
+    exit 1
 }
 
-# Create aliases
+# Create functions that map to specific nav.py calls
+function apps() { cd `nav.py apps` ; }
+function cyghome() { cd `nav.py cyghome` ; }
+function desktop() { cd `nav.py desktop` ; }
+function documents() { cd `nav.py documents` ; }
+function downloads() { cd `nav.py downloads` ; }
+function dropbox() { cd `nav.py dropbox` ; }
+function extdrive() { cd `nav.py extdrive` ; }
+function gdrive() { cd `nav.py gdrive` ; }
+function hd() { cd `nav.py hd` ; }
+function home() { cd `nav.py home` ; }
+function movies() { cd `nav.py movies` ; }
+function music() { cd `nav.py music` ; }
+function pictures() { cd `nav.py pictures` ; }
+function public() { cd `nav.py public` ; }
+function systembin() { cd `nav.py systembin` ; }
+function userapps() { cd `nav.py userapps` ; }
+function userbin() { cd `nav.py userbin` ; }
+function googledrive() { cd `nav.py gdrive` ; }
+function google_drive() { cd `nav.py gdrive` ; }
+function mydocuments() { cd `nav.py documents` ; }
+function my_documents() { cd `nav.py documents` ; }
+function mymusic() { cd `nav.py music` ; }
+function my_music() { cd `nav.py music` ; }
+function mypictures() { cd `nav.py pictures` ; }
+function my_pictures() { cd `nav.py pictures` ; }
+function myvideos() { cd `nav.py movies` ; }
+function my_videos() { cd `nav.py movies` ; }
+function videos() { cd `nav.py movies` ; }
+function extvol() { cd `nav.py extdrive` ; }
+function extvolume() { cd `nav.py extdrive` ; }
